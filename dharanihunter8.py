cae1097d5c0fc47a45f0a9b7d1746dea958bf960
dharanianbu <52280951+dharanianbu@users.noreply.github.com>
2019-08-14 16:10:22 +0530

    Create dharanihunter8.py

---
+++ dharanihunter8.py
@@ -0,0 +1,8 @@
+#d
+null = input()
+l = list(map(int,input().split()))
+for i in range(len(l)):
+    for j in range(len(l)):
+        for k in range(len(l)):
+            if l[i]+l[j] == l[k] and i<j<k:
+                print(l[i],l[j],l[k])
